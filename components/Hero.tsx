8510aeb589bdc5f0ea3b51749656c5adf84a4425
Stephen <scpwalker@hotmail.com>
2024-05-23 16:33:01 +0100

    added title to page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Crafting Dynamic Web Experiences
           </h2>
 
           <TextGenerateEffect
