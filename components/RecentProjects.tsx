7cfde95d54a141b17422c7da8e5ab52409429469
Stephen <scpwalker@hotmail.com>
2024-05-29 07:55:28 +0100

    title added and styled for projects

---
+++ components/RecentProjects.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RecentProjects = () => {
+  return (
+    <div className="py-20">
+      <h1 className="heading">
+        A selection of my {""}
+        <span className="text-purple">recent projects</span>
+      </h1>
+    </div>
+  );
+};
+
+export default RecentProjects;
