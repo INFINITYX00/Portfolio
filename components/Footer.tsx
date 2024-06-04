59bdedb996493cbff6bc57552e546aaf3d5a0f83
Stephen <scpwalker@hotmail.com>
2024-06-04 09:08:50 +0100

    footer buttons added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+import { socialMedia } from "../data";
 
 const Footer = () => {
   return (
@@ -14,6 +17,33 @@ const Footer = () => {
         <h1 className="heading lg:max-w-[45vw]">
           Connect with <span className="text-purple">Me</span>
         </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          I am always open to new opportunities and collaborations. Feel free to
+          reach out to me.
+        </p>
+        <a href="mailto:scpwalker@hotmail.com">
+          <MagicButton
+            title="Get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Stephen Walker
+        </p>
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <div
+              key={profile.id}
+              className="w-10 h-10 cursor-pointer flex
+            justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={profile.img} alt={profile.id} width={20} height={20} />
+            </div>
+          ))}
+        </div>
       </div>
     </footer>
   );
